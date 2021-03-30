48df48236439793048a2954ebc154af6f5f5399d
Lucas de Almeida Sarmento <lukas.barcee@gmail.com>
2021-03-30 13:52:49 -0300

    Refatorando

--- script.js
+++ script.js
@@ -1,58 +1,101 @@
-// Animação ao carregar a página
-function animationPreLoad(){     
-    document.getElementById("icon-background").classList.remove("pre-load");
-    document.getElementById("icon-header").classList.remove("pre-load");
-    document.getElementById("h1-header").classList.remove("pre-load");
-    document.getElementById("p-header").classList.remove("pre-load");
+// Elementos do Header
+const iconBackground = document.getElementById("icon-background")
+const iconHeader = document.getElementById("icon-header")
+const titleHeader = document.getElementById("h1-header")
+const subtitleHeader = document.getElementById("p-header")
+
+
+
+// Função auxiliar para adicionar ou remover classes
+function AddRemoveClass(bool ,className, ...elements){    
+  if(bool){
+    for (const element of elements) {
+      element.classList.add(className)
+    }
+  }else{
+    for (const element of elements) {
+      element.classList.remove(className)
+    }
+  }
 }
-onload = animationPreLoad();
+
+
+
+
+// Animação ao carregar a página
+onload = function animationPreLoad(){      
+  AddRemoveClass(false, "pre-load", iconBackground, iconHeader, titleHeader, subtitleHeader)
+}
+
+
 
 
 // Trabalhando com o menu escondido
 // Capturando elementos
-const check = document.querySelector("#hamburguer-check");
-const menu = document.getElementById("container-menu");
-const header = document.getElementById("header-top");
-const main = document.getElementById("main-content");
-const footer = document.getElementById("footer");
+const check = document.querySelector("#hamburguer-check")
+const menu = document.getElementById("container-menu")
+const header = document.getElementById("header-top")
+const main = document.getElementById("main-content")
+const footer = document.getElementById("footer")
 
-// Exibindo Menu
-function showMenu(bool){
+
+// Função que exibe ou oculta o menu
+function hideShowMenu(bool){
   if(bool){
-    header.style.filter = "blur(10px)";
-    main.style.filter = "blur(10px)";
-    footer.style.filter = "blur(10px)";
-    menu.classList.remove("hide");
+    toggleBlur(true, 10, header, main, footer)   
+    AddRemoveClass(false, "hide", menu)
   }else{
-    header.style.filter = "blur(0px)";
-    main.style.filter = "blur(0px)";
-    footer.style.filter = "blur(0px)";
-    menu.classList.add("hide");
+    toggleBlur(false, 0, header, main, footer)    
+    AddRemoveClass(true, "hide", menu)
   }
 }
 
+// Adiciona ou remove o blur nos elementos
+function toggleBlur(bool, pixel, ...elements){
+    if (bool) {
+      for(const element of elements){
+        element.style.filter = "blur("+pixel+"px)"
+      }
+    }else{
+      for(const element of elements){
+        element.style.filter = "blur(0px)"
+      }
+    }
+}
+
+// Evento ao marcar ou desmarcar meu checkbox do menu
 check.addEventListener("change", function (e) {
     if (check.checked) {
-       showMenu(true);
+      hideShowMenu(true);
     }else{
-      showMenu(false);
+      hideShowMenu(false);
     }
 })
 
 
+
+
+
+
+
 //Caso eu redimensione a tela para um tamanho maior que 426px enquanto meu menu-hamburguer esteja ativo.
 
-function tamanhoDaTela(){    
-    var windowWidth = window.innerWidth;      
-    return windowWidth; // Capturo o tamanho da janela atual
-}
+function getCurrentScreenSize() {
+  var widthWindow = window.innerWidth; 
+  return widthWindow; // Capturo o tamanho da janela atual
+}       
+    
+
 window.addEventListener('resize', function() {
-   var larguraTela = tamanhoDaTela();
-   if (larguraTela > 426) {
+   let currentScreenSize = getCurrentScreenSize()
+   
+   if (currentScreenSize > 426) {
         check.checked = 0;
-        showMenu(false);
+        hideShowMenu(false);
    }   
 });
+
+
 
 
 
@@ -63,8 +106,7 @@ $('#nav-top a').on('click', function (e) {
     e.preventDefault();
     const hash = this.hash;
 
-    $('html, body')
-      .animate({
+    $('html, body').animate({
         scrollTop: $(hash).offset().top - 68
       },800);
   }
@@ -75,13 +117,12 @@ $('#container-menu a').on('click', function (e) {
       e.preventDefault();  
       const hash = this.hash;
   
-      $('html, body')
-        .animate({
+      $('html, body').animate({
           scrollTop: $(hash).offset().top - 10
         },800);
 
         check.checked = 0;
-        showMenu(false);
+        hideShowMenu(false);
     }
   });
 
